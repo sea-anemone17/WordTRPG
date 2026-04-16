ca3a06a23901add011be9ec6f68580eb1fa43f47
sea-anemone17 <leehaeju09@gmail.com>
2026-04-16 20:14:53 +0900

    Update index.js

--- scenarios/index.js
+++ scenarios/index.js
@@ -1,9 +1,13 @@
 import { closedCorridorScenario } from "./closed_corridor.js";
 import { archiveBeneathDustScenario } from "./archive_beneath_dust.js";
 import { signalloststationScenario } from "./signal_lost_station.js";
+import { citynoisewalkScenario } from "./city_noise_walk.js";
+import { forestunmarkedpathScenario } from "./forest_unmarked_path.js";
 
 export const scenarios = [
   closedCorridorScenario,
   archiveBeneathDustScenario,
-  signalloststationScenario
+  signalloststationScenario,
+  citynoisewalkScenario,
+  forestunmarkedpathScenario
 ];
