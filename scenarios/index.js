f534382acf5a0b9d6cb45825349b31d79a0cfbb3
sea-anemone17 <leehaeju09@gmail.com>
2026-04-16 20:10:42 +0900

    Update index.js

--- scenarios/index.js
+++ scenarios/index.js
@@ -1,7 +1,9 @@
 import { closedCorridorScenario } from "./closed_corridor.js";
 import { archiveBeneathDustScenario } from "./archive_beneath_dust.js";
+import { signalloststationScenario } from "./signal_lost_station.js";
 
 export const scenarios = [
   closedCorridorScenario,
-  archiveBeneathDustScenario
+  archiveBeneathDustScenario,
+  signalloststationScenario
 ];
